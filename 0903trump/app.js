b45095c41b917406259c42331cb5b805bd0a5db7
masaki <22110023@oic-ok.ac.jp>
2024-09-03 13:30:03 +0900

    b

--- 0903trump/app.js
+++ 0903trump/app.js
@@ -1,34 +1,92 @@
+
+
 async function drawCard() {
-    const response = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
+    // ドロー
+    const response = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=2');
     const data = await response.json();
   
-    if (data.success) {
-      const card = data.cards[0];
-      const cardImage = document.getElementById('cardImage');
-      cardImage.src = card.image;
-      cardImage.style.display = 'block';
-        
-        // 取得カードの値表示
-        const cardValue = document.getElementById('cardValue');
-        switch (card.value) {
-            case "ACE" :
-                cardValue.textContent = `カードの値: 1`;
-                break;
-            case "JACK" :
-                cardValue.textContent = `カードの値: 11`;
-                break;
-            case "QUEEN" :
-                cardValue.textContent = `カードの値: 12`;
-                break;
-            case "KING" :
-                cardValue.textContent = `カードの値: 13`;
-                break;
-            default :
-                cardValue.textContent = `カードの値: ${card.value}`;
-                break;
-        }
-        
-    } else {
-      console.error('カードの取得に失敗しました');
+    // 表示カード
+    const card = data.cards[0];
+    const cardImage = document.getElementById('cardImage');
+    cardImage.src = card.image;
+    cardImage.style.display = 'block';
+
+    // 非表示カード
+    const hideCard = data.cards[1];
+    // const hideCardImage = document.getElementById('hideCardImage');
+    // hideCardImage.src = hideCard.image;
+    hideCardImage.style.display = 'block';
+    
+    // 取得カードの値表示
+    const cardValue = document.getElementById('cardValue');
+    const hideCardValue = document.getElementById('hideCardValue');
+    // 値変換
+    switch (card.value) {
+        case "ACE" :
+            cardValue.textContent = `1`;
+            break;
+        case "JACK" :
+            cardValue.textContent = `11`;
+            break;
+        case "QUEEN" :
+            cardValue.textContent = `12`;
+            break;
+        case "KING" :
+            cardValue.textContent = `13`;
+            break;
+        default :
+            cardValue.textContent = `${card.value}`;
+            break;
     }
-  }
+    switch (hideCard.value) {
+        case "ACE" :
+            hideCardValue.textContent = `1`;
+            break;
+        case "JACK" :
+            hideCardValue.textContent = `11`;
+            break;
+        case "QUEEN" :
+            hideCardValue.textContent = `12`;
+            break;
+        case "KING" :
+            hideCardValue.textContent = `13`;
+            break;
+        default :
+            hideCardValue.textContent = `${hideCard.value}`;
+            break;
+    }
+}
+
+// Highボタン
+async function highBtn() {
+    // pタグの数値を取得
+    let card = parseInt(document.getElementById("cardValue").textContent);
+    let hideCard = parseInt(document.getElementById("hideCardValue").textContent);
+    
+    if (card < hideCard) {
+        drawCard();
+        alert("○");
+    } else {
+        
+        alert("×");
+    }
+}
+
+// Lowボタン
+async function lowBtn() {
+    // pタグの数値を取得
+    let card = parseInt(document.getElementById("cardValue").textContent);
+    let hideCard = parseInt(document.getElementById("hideCardValue").textContent);
+
+    if (card > hideCard) {
+        alert("○");
+        drawCard();
+    } else {
+        alert("×");
+    }
+}
+
+// リセットボタン
+async function resetBtn() {
+    location.reload();
+}
