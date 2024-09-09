9eb64ccac2e5e28d88745b7abb2f66043e4d5fc3
masaki <22110023@oic-ok.ac.jp>
2024-09-09 14:13:42 +0900

    aiueo

--- 0903trump/app.js
+++ 0903trump/app.js
@@ -1,5 +1,9 @@
+// カード識別用
+var cardNo = 1;
+
 const apiUrl = "https://deckofcardsapi.com/api/deck/new/draw/?count=20"; // 一度に20枚取得
 
+// 手札
 document.getElementById("get").addEventListener("click", async () => {
   const cardDiv = document.getElementById("card");
   cardDiv.innerHTML = ""; // 前回の結果をクリア
@@ -14,6 +18,11 @@ document.getElementById("get").addEventListener("click", async () => {
     // カードの画像
     const image = document.createElement("img");
     image.src = card.image;
+    // カードイベント識別
+    image.setAttribute("id", "card" + cardNo);
+    // カード選択
+    image.setAttribute("onclick", "cardSelect" + cardNo + "()");
+    cardNo++;
 
     // カードの値
     const value = document.createElement("p");
@@ -29,6 +38,7 @@ document.getElementById("get").addEventListener("click", async () => {
   });
 });
 
+// お題をランダムで表示
 function random() {
     //1か2の乱数生成
     var num = Math.floor(Math.random() * 2 + 1); 
@@ -44,3 +54,46 @@ function random() {
     document.getElementById("ransuu").innerHTML = num; 
 }
 
+// カード選択1~10枚目
+function cardSelect1() {
+  const no = document.getElementById("card1");
+  no.style.width = '400px';
+  alert("取得：1");
+}
+function cardSelect2() {
+  var no = document.getElementById("card2");
+  no.style.width = '400px';
+  alert("取得：2");
+}
+function cardSelect3() {
+  var no = document.getElementById("card3");
+  alert("取得：3");
+}
+function cardSelect4() {
+  var no = document.getElementById("card4");
+  alert("取得：4");
+}
+function cardSelect5() {
+  var no = document.getElementById("card5");
+  alert("取得：5");
+}
+function cardSelect6() {
+  var no = document.getElementById("card6");
+  alert("取得：6");
+}
+function cardSelect7() {
+  var no = document.getElementById("card7");
+  alert("取得：7");
+}
+function cardSelect8() {
+  var no = document.getElementById("card8");
+  alert("取得：8");
+}
+function cardSelect9() {
+  var no = document.getElementById("card9");
+  alert("取得：9");
+}
+function cardSelect10() {
+  var no = document.getElementById("card10");
+  alert("取得：10");
+}
