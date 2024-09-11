a330fc3c4b16993cecefa40ea3fbe4be3e7b2499
masaki <22110023@oic-ok.ac.jp>
2024-09-11 09:47:33 +0900

    aieuo

--- 0903trump/app.js
+++ 0903trump/app.js
@@ -1,15 +1,10 @@
 // カード識別用
 var cardNo = 1;
 let cards = [];
-var apiUrl = "";
-
-window.onload = function() {
-  apiUrl = "https://deckofcardsapi.com/api/deck/new/draw/?count=20"; // 一度に20枚取得
-};
+const apiUrl = "https://deckofcardsapi.com/api/deck/new/draw/?count=20"; // 一度に20枚取得
 
 
-// 手札
-document.getElementById("get").addEventListener("click", async () => {
+window.onload = async function() {
   const cardDiv = document.getElementById("card");
   cardDiv.innerHTML = ""; // 前回の結果をクリア
   cardNo = 1;
@@ -103,6 +98,90 @@ document.getElementById("get").addEventListener("click", async () => {
   
       document.getElementById("result").innerHTML = result; 
       document.getElementById("ransuu").innerHTML = num; 
+};
+
+
+// // 手札
+document.getElementById("get").addEventListener("click", async () => {
+  const cardDiv = document.getElementById("card");
+  cardDiv.innerHTML = ""; // 前回の結果をクリア
+  cardNo = 1;
+
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  // カード情報を取得
+  cards = data.cards;
+
+  cards.forEach(card => {
+    // カードの画像
+    const image = document.createElement("img");
+    image.src = card.image;
+    // カードイベント識別
+    image.setAttribute("id", "card" + cardNo);
+    // カード選択
+    image.setAttribute("onclick", "cardSelect" + cardNo + "()");
+    cardNo++;
+
+    // カードの値
+    const value = document.createElement("p");
+    value.textContent = `Card: ${card.value} of ${card.suit}`;
+
+    // カードコンテナに追加
+    const cardContainer = document.createElement("div");
+    cardContainer.style.marginBottom = "20px";
+    cardContainer.appendChild(image);
+    cardContainer.appendChild(value);
+
+    cardDiv.appendChild(cardContainer);
+  });
+
+  // CPU画像配置IDの取得
+  cpuCardId1 = document.getElementById("cpuCardId1");
+  cpuCardId2 = document.getElementById("cpuCardId2");
+  cpuCardId3 = document.getElementById("cpuCardId3");
+  cpuCardId4 = document.getElementById("cpuCardId4");
+  cpuCardId5 = document.getElementById("cpuCardId5");
+  cpuCardId6 = document.getElementById("cpuCardId6");
+  cpuCardId7 = document.getElementById("cpuCardId7");
+  cpuCardId8 = document.getElementById("cpuCardId8");
+  cpuCardId9 = document.getElementById("cpuCardId9");
+  cpuCardId10 = document.getElementById("cpuCardId10");
+  // 取得IDにトランプ
+  cpuCardId1.src = cards[10].image;
+  cpuCardId2.src = cards[11].image;
+  cpuCardId3.src = cards[12].image;
+  cpuCardId4.src = cards[13].image;
+  cpuCardId5.src = cards[14].image;
+  cpuCardId6.src = cards[15].image;
+  cpuCardId7.src = cards[16].image;
+  cpuCardId8.src = cards[17].image;
+  cpuCardId9.src = cards[18].image;
+  cpuCardId10.src = cards[19].image;
+
+
+  // プレイヤー画像配置IDの取得
+  playCardId1 = document.getElementById("playCardId1");
+  playCardId2 = document.getElementById("playCardId2");
+  playCardId3 = document.getElementById("playCardId3");
+  playCardId4 = document.getElementById("playCardId4");
+  playCardId5 = document.getElementById("playCardId5");
+  playCardId6 = document.getElementById("playCardId6");
+  playCardId7 = document.getElementById("playCardId7");
+  playCardId8 = document.getElementById("playCardId8");
+  playCardId9 = document.getElementById("playCardId9");
+  playCardId10 = document.getElementById("playCardId10");
+  // 取得IDにトランプ
+  playCardId1.src = cards[0].image;
+  playCardId2.src = cards[1].image;
+  playCardId3.src = cards[2].image;
+  playCardId4.src = cards[3].image;
+  playCardId5.src = cards[4].image;
+  playCardId6.src = cards[5].image;
+  playCardId7.src = cards[6].image;
+  playCardId8.src = cards[7].image;
+  playCardId9.src = cards[8].image;
+  playCardId10.src = cards[9].image;
 });
 
 // カード選択1~10枚目
